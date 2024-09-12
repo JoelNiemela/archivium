fed92e77831fae427f4986e3d8f8daff26b983e3
Johannes Niemelä <johannes@niemela.se>
2024-09-12 10:45:15 -0500

    add sort select to item list

--- static/scripts/listUtils.js
+++ static/scripts/listUtils.js
@@ -17,4 +17,20 @@ window.addEventListener('load', () => {
       toggleFilter(filter.dataset.filterKey, filter.dataset.filterValue);
     };
   });
+  
+  const url = new URL(window.location);
+  if (url.searchParams.has('sort')) {
+    document.getElementById('sort').value = url.searchParams.get('sort');
+    document.getElementById('sort_order').value = url.searchParams.get('sort_order');
+  }
+
+  const handleSort = () => {
+    const url = new URL(window.location);
+    url.searchParams.set('sort', document.getElementById('sort').value);
+    url.searchParams.set('sort_order', document.getElementById('sort_order').value);
+    history.pushState(null, '', url);
+    location.reload();
+  };
+  document.getElementById('sort').onchange = handleSort;
+  document.getElementById('sort_order').onchange = handleSort;
 });

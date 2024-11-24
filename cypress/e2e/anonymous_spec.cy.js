04c34df64046bc4dc33df65dc5ef2d6ad7844729
kjniemela <johannes@niemela.se>
2024-11-23 19:50:14 -0600

    anonymous spec

--- cypress/e2e/anonymous_spec.cy.js
+++ cypress/e2e/anonymous_spec.cy.js
@@ -1,13 +1,48 @@
 describe('Anonymous user spec', () => {
   before(() => {
-    cy.clearCookies();
+    cy.logout();
   });
 
-  it('vists unprotected pages, and ', () => {
+  it('vists home page and sees welcome message', () => {
     cy.visit('/');
 
+    cy.get('h1').contains('Welcome to Archivium').should('exist');
     cy.get('.navbarBtnLink').contains('Log In').should('have.attr', 'href', '/login');
     cy.get('.navbarBtnLink').contains('Create Account').should('have.attr', 'href', '/signup?page=%2Funiverses%2Fcreate');
+  });
+
+  it('vists unprotected pages and doesn\'t get redirected', () => {
+    const pages = [
+      '/universes',
+      '/search',
+      '/universes/public-test-universe',
+      '/universes/public-test-universe/items',
+      '/universes/public-test-universe/items/test-character',
+    ];
+    for (const page of pages) {
+      cy.visit(page);
+      cy.url().should('include', Cypress.config().baseUrl + page);
+    }
+  });
+
+  it('vists protected pages and gets redirected', () => {
+    const pages = [
+      '/contacts',
+      '/universes/create',
+      '/universes/public-test-universe/items/create',
+      '/universes/public-test-universe/items/test-parent/edit',
+      '/universes/chatroom/discuss/create'
+    ];
+    for (const page of pages) {
+      cy.visit(page);
+      cy.url().should('include', Cypress.config().baseUrl + '/login');
+    }
+  });
+
+  it('checks that new item buttons are not visible', () => {
+    cy.visit('/universes/public-test-universe');
+
+    cy.get('.item-type a').contains('Characters').parent().parent().parent().contains('New').should('not.exist');
   });
 });
   

7b07124b828f0ab62c9dedad9885dbadc6b219e2
Johannes Niemelä <johannes@niemela.se>
2024-11-29 12:18:47 -0600

    update how snoozing works

--- views.js
+++ views.js
@@ -65,11 +65,13 @@ module.exports = function(app) {
       }) : [200, []];
       res.status(code3);
       const [code4, oldestUpdated] = await api.item.getMany(user, null, perms.WRITE, {
-        sort: 'updated_at',
+        sort: `GREATEST(IFNULL(snooze.snoozed_at, '1000-01-01'), IFNULL(item.updated_at, '1000-01-01'))`,
         sortDesc: false,
+        forceSort: true,
         limit: 16,
         join: [['LEFT', 'snooze', new Cond('snooze.item_id = item.id').and('snooze.snoozed_by = ?', user.id)]],
-        where: new Cond('snooze.snoozed_until < NOW()').or('snooze.snoozed_until IS NULL').and('item.updated_at < DATE_SUB(NOW(), INTERVAL 2 DAY)'),
+        where: new Cond('item.updated_at < DATE_SUB(NOW(), INTERVAL 2 DAY)'),
+        groupBy: ['snooze.snoozed_at'],
       });
       res.status(code4);
       if (!oldestUpdated) return;

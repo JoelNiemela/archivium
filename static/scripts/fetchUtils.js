8392c4844a1637daca04e99430debda2da890fec
Johannes Niemelä <johannes@niemela.se>
2024-12-11 20:20:44 -0600

    remove example code from fetchutils

--- static/scripts/fetchUtils.js
+++ static/scripts/fetchUtils.js
@@ -44,15 +44,3 @@ async function deleteJSON(url) {
   })
     .then(response => response.json());
 }
-
-async function fetchJSON(url) {
-  return await fetch('https://example.com/api/resource', {
-    method: 'PUT', // Specify the request method as PUT
-    headers: {
-      'Content-Type': 'application/json', // Set the content type to JSON
-      // Add any additional headers you need, such as authorization tokens
-    },
-    body: JSON.stringify({ key: 'value' }) // Replace with the data you want to send
-  })
-    .then(response => response.json());
-}

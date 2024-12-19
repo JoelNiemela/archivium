9f11308996533f138e9cfad0488bf970c028e8f1
Johannes Niemelä <johannes@niemela.se>
2024-12-19 16:20:37 -0600

    force username validation

--- templates/index.js
+++ templates/index.js
@@ -1,6 +1,7 @@
 const pug = require('pug');
 const { ADDR_PREFIX } = require('../config');
 const { perms } = require('../api/utils');
+const api = require('../api');
 const md5 = require('md5');
 const path = require('path');
 
@@ -61,6 +62,7 @@ function contextData(req) {
     perms,
     locale: locale[lang],
     T,
+    validateUsername: api.user.validateUsername,
   };
 }
 
